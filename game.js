75437c6b9704922f5b887963de9fa611fe348637
stephancode <streangasarmis@gmail.com>
2017-04-12 23:46:37 +0300

    Added some stuff

--- game.js
+++ game.js
@@ -2,8 +2,16 @@
 var canvas;
 var context;
 
+var camera;
+
 var gary;
 var blocks = [];
+var enemies = [];
+
+var the_one_and_only_audio = new Audio("DONT FUCKING STOP ME.mp3");
+the_one_and_only_audio.loop = true;
+
+var score = 0;
 
 var Key = {
 	pressed: {},
@@ -14,44 +22,59 @@ var Key = {
 };
 
 window.onload = function(){
-	
+    the_one_and_only_audio.play();
+
 	canvas = document.getElementById("game_canvas");
 	context = canvas.getContext("2d");
 	setInterval(main_loop, 1000 / 60);
 	window.addEventListener("keydown", function(event){Key.onKeyDown(event);}, false);
 	window.addEventListener("keyup", function(event){Key.onKeyUp(event);}, false);
-	
+
 	gary = new Gary();
-	for(var x = 0; x < canvas.width / 20; x++) blocks.push(new Block(x * 20, canvas.height - 20));
-	for(var x = 5; x < canvas.width / 20; x++) blocks.push(new Block(x * 20, canvas.height - 40));
-	for(var x = 10; x < canvas.width / 20; x++) blocks.push(new Block(x * 20, canvas.height - 60));
+    camera = new Camera();
+    generate_chunk();
+    // for(var y = 0; y < level1.length; y++){
+    //     for(var x = 0; x < level1[0].length; x++) {
+	//   	if(level1[y][x] == 1)
+    //     		blocks.push(new Block(x * 40, y * 40));
+    //     }
+    // }
+
 }
 
 function main_loop(){
 	// update
 	gary.update();
-
+	camera.update();
+    for(var i = 0; i < enemies.length; i++){
+        enemies[i].update();
+    }
 	// render
+
 	context.fillStyle = "#ffffff"
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
 	for(var i = 0; i < blocks.length; i++){
 		blocks[i].render();
 	}
 
-	gary.render();
-	
+    for(var i = 0; i < enemies.length; i++){
+        enemies[i].render();
+    }
+	if(Math.floor(Math.random() * 5) == 0) enemies.push(new TypicalEnemy(Math.random() * 10000 * 40, canvas.height - 100));
+    gary.render();
+
 }
 
 function Gary(){
 	
-	context.font = "20px Times New Roman";
-	
+
 	var right_stance = "ᕕ( ᐛ )ᕗ";
 	var left_stance = "ᕕ( ᐕ )ᕗ";
-	var current_stance = "";
-	
-	this.x = 0;
+	var current_stance = "ᕕ( ᐛ )ᕗ";
+    var trail = "三";
+
+    this.x = 100;
 	this.y = 20;
 	this.velx = 0;
 	this.vely = 0;
@@ -59,7 +82,7 @@ function Gary(){
 	var gravity = 0;
 	var grav_acc = 0.08;
 	var speed = 0.1;
-	var speed_cap = 5;
+	var speed_cap = 20;
 	var timer = 0;
 	var jump_vel = 0;
 	
@@ -68,7 +91,7 @@ function Gary(){
 		timer %= 60;
 		
 		var grounded = this.on_ground(this.vely);
-	
+
 		if(!grounded){
 			gravity += grav_acc;
 			this.vely += gravity;
@@ -115,6 +138,7 @@ function Gary(){
 	
 	this.can_pass = function(xa, ya){
 		var x0 = this.x + xa;
+		if(x0 < 0) return false;
 		var x1 = this.x + current_stance.length * 10 + xa;
 		var y0 = this.y - 20 + ya;
 		var y1 = this.y + ya;
@@ -130,7 +154,7 @@ function Gary(){
 		var x0 = this.x;
 		var x1 = this.x + current_stance.length * 10;
 		var y0 = this.y + ya;
-		var y1 = y0 + 5;
+		var y1 = y0 + 1;
 
 		for(var i = 0; i < blocks.length; i++){
 			if(x0 < blocks[i].x1 && x1 > blocks[i].x0 &&
@@ -140,8 +164,19 @@ function Gary(){
 	}
 	
 	this.render = function(){
-		context.fillStyle = "#000000";
-		context.fillText(current_stance, this.x, this.y);
+        context.font = "20px Arial";
+        context.fillStyle = "#000000";
+        if(this.velx > speed_cap / 2)
+            context.fillText(trail, this.x - camera.x - 20, this.y - camera.y);
+        if(this.velx < -speed_cap / 2)
+            context.fillText(trail, this.x - camera.x + 70, this.y - camera.y);
+        if(this.velx == speed_cap)
+            context.fillText(trail, this.x - camera.x - 20 * 2, this.y - camera.y);
+        if(this.velx == -speed_cap)
+            context.fillText(trail, this.x - camera.x + 70 + 20, this.y - camera.y);
+
+        context.fillText("Craziness: " + score, 20, 20);
+		context.fillText(current_stance, this.x - camera.x, this.y - camera.y);
 	}
 }
 
@@ -149,15 +184,170 @@ function Block(x, y){
 	
 	this.x = x;
 	this.y = y;
-	this.w = 20;
-	this.h = 20;
-	this.x0 = this.x;
-	this.x1 = this.x + this.w;
-	this.y0 = this.y - this.h;
-	this.y1 = this.y;
-	
+	this.w = 40;
+	this.h = 40;
+    this.x0 = this.x;
+    this.x1 = this.x + this.w;
+    this.y0 = this.y - this.h;
+    this.y1 = this.y;
+
 	this.render = function(){
-		context.fillStyle = "#000000";
-		context.fillText(String.fromCharCode(0xe296a9), this.x, this.y);
+        context.font = "40px Arial";
+        context.fillStyle = "#000000";
+		context.fillText(String.fromCharCode(0xe296a9), this.x - camera.x, this.y - camera.y);
 	}
 }
+
+function TypicalEnemy(x, y){
+
+	var strings = [
+		"ง ( ⌓̈ )ง", "(☞ ᐛ )☞", "(☜ ᐖ )☜", "(´･ω･`)", "( ´･ω･)", "(・ω・)b"
+	];
+
+	var random_index = (Math.random() * strings.length) | 0;
+
+	this.can_collide = true;
+	this.x = x;
+    this.y = y;
+    this.velx = 0;
+    this.vely = 0;
+
+    var gravity = 0;
+    var grav_acc = 0.08;
+    var jump_vel = 0;
+
+    var once = 0;
+
+    this.update = function(){
+        var grounded = this.on_ground(this.vely);
+
+        if(!grounded){
+            gravity += grav_acc;
+            this.vely += gravity;
+        } else {
+            gravity = 0;
+            this.vely = 0;
+        }
+
+        if(!this.touch_player()){
+			if(random_index % 2 == 0) this.velx = 2;
+			else this.velx = -2;
+		} else {
+            this.can_collide = false;
+            jump_vel -= 2;
+            this.vely += jump_vel;
+            if(this.velx != 0 || this.vely != 0) this.move(this.velx, this.vely);
+            if(once++ == 0)score++;
+            return;
+		}
+        if(gravity > 20) gravity = 20;
+        if(this.velx != 0 || this.vely != 0) this.move(this.velx, this.vely);
+    }
+
+
+    this.move = function(xa, ya){
+        if(xa != 0 && ya != 0){
+            this.move(xa, 0);
+            this.move(0, ya);
+            return;
+        }
+        if(this.can_pass(xa, ya)){
+            this.x += xa;
+            this.y += ya;
+        }
+    }
+
+    this.can_pass = function(xa, ya){
+    	if(this.can_collide) {
+            var x0 = this.x + xa;
+            if (x0 < 0) return false;
+            var x1 = this.x + strings[random_index].length * 10 + xa;
+            var y0 = this.y - 20 + ya;
+            var y1 = this.y + ya;
+
+            for (var i = 0; i < blocks.length; i++) {
+                if (x0 < blocks[i].x1 && x1 > blocks[i].x0 &&
+                    y0 < blocks[i].y1 && y1 > blocks[i].y0) return false;
+            }
+        }
+        return true;
+    }
+
+    this.on_ground = function(ya){
+    	if(this.can_collide) {
+            var x0 = this.x;
+            var x1 = this.x + strings[random_index].length * 10;
+            var y0 = this.y + ya;
+            var y1 = y0 + 1;
+
+            for (var i = 0; i < blocks.length; i++) {
+                if (x0 < blocks[i].x1 && x1 > blocks[i].x0 &&
+                    y0 < blocks[i].y1 && y1 > blocks[i].y0) return true;
+            }
+        }
+        return false;
+    }
+
+    this.touch_player = function(){
+        var x0 = this.x;
+        var x1 = this.x + strings[random_index].length * 10;
+        var y0 = this.y - 20;
+        var y1 = this.y;
+
+        var gx0 = gary.x;
+        var gx1 = gary.x + 7 * 10;
+        var gy0 = gary.y - 20;
+        var gy1 = gary.y;
+
+		if(x0 < gx1 && x1 > gx0 && y0 < gy1 && y1 > gy0) return true;
+		return false;
+	}
+
+    this.render = function(){
+        context.font = "20px Arial";
+        context.fillStyle = "#000000";
+        context.fillText(strings[random_index], this.x - camera.x, this.y - camera.y);
+    }
+
+
+}
+
+function Camera(){
+
+	this.x = 0;
+    this.y = 0;
+
+    this.update = function(){
+        this.x = gary.x - canvas.width / 2;
+        this.y = gary.y - canvas.height / 2;
+    }
+
+}
+
+function generate_chunk(){
+	for(var i = 1; i <= 1000; i++){
+		if(i % 20 == 0) enemies.push(new TypicalEnemy(i * 40, canvas.height - 100));
+		blocks.push(new Block(i * 40, canvas.height - 40));
+	}
+
+	for(var i = 0; i <= 10; i++){
+        blocks.push(new Block(0, canvas.height - 40 + i * -40));
+	}
+
+    for(var i = 0; i <= 10; i++){
+        blocks.push(new Block(1000 * 40, canvas.height - 40 + i * -40));
+    }
+}
+
+/************LEVELS********************/
+
+// var level1 =
+// 	[[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+// 	 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+// 	 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+// 	 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+// 	 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+// 	 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+// 	 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+// 	 [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+// 	 [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]];
